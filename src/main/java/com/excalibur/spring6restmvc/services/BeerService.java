ed82258d5ba5f4c703dba535920ea8eb141e44bb
Arthur Queiroz <arthutqueiroz@hotmail.com>
2024-02-21 08:14:23 -0300

    List example

--- src/main/java/com/excalibur/spring6restmvc/services/BeerService.java
+++ src/main/java/com/excalibur/spring6restmvc/services/BeerService.java
@@ -2,9 +2,12 @@ package com.excalibur.spring6restmvc.services;
 
 import com.excalibur.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+
+    List<Beer> listBeers();
 
     Beer getBeerById(UUID id);
 }
