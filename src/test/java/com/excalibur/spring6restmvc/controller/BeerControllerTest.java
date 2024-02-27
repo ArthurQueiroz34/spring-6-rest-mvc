8a3eb595bf7f124b7f64873eda492be8cd159b92
Arthur Queiroz <arthutqueiroz@hotmail.com>
2024-02-27 17:06:11 -0300

    Creating JSON using Jackson

--- src/test/java/com/excalibur/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/excalibur/spring6restmvc/controller/BeerControllerTest.java
@@ -1,5 +1,7 @@
 package com.excalibur.spring6restmvc.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.excalibur.spring6restmvc.model.Beer;
 import com.excalibur.spring6restmvc.services.BeerService;
 import com.excalibur.spring6restmvc.services.BeerServiceImpl;
@@ -21,10 +23,21 @@ class BeerControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @MockBean
     BeerService beerService;
 
     BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
+
+    @Test
+    void testCreateNewBeer() throws JsonProcessingException {
+        Beer beer = beerServiceImpl.listBeers().get(0);
+
+        System.out.println(objectMapper.writeValueAsString(beer));
+
+    }
 
     @Test
     void testListBeers() throws Exception {
